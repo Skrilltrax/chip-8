c2e142ec5b716c05a0f1b738d84bc949a6cd377b
Aditya Wasan <adityawasan55@gmail.com>
2022-01-15 17:26:06 +0530

    feat(desktop): use `Render` composable to render pixels on screen
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,12 +1,26 @@
-import dev.skrilltrax.chip8.App
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import dev.skrilltrax.chip8.Render
+import dev.skrilltrax.chip8.model.DisplayMatrix
 
 fun main() = application {
   Window(onCloseRequest = ::exitApplication) {
     MaterialTheme {
-      App()
+      val displayMatrix = remember { DisplayMatrix.createEmpty() }
+      SideEffect {
+        displayMatrix.setPixel(14, 15)
+        displayMatrix.setPixel(14, 16)
+        displayMatrix.setPixel(14, 17)
+        displayMatrix.setPixel(14, 18)
+        displayMatrix.setPixel(14, 19)
+        displayMatrix.setPixel(14, 20)
+      }
+      Render(Modifier.fillMaxSize(), displayMatrix, 20)
     }
   }
 }
