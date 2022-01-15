e1dabb947a9640e5ba1e69a18d3c7e6187d89090
Aditya Wasan <adityawasan55@gmail.com>
2022-01-15 17:25:12 +0530

    feat(renderer): add `Render` composable to render pixels on screen
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- common/src/commonMain/kotlin/dev/skrilltrax/chip8/Renderer.kt
+++ common/src/commonMain/kotlin/dev/skrilltrax/chip8/Renderer.kt
@@ -1,2 +1,29 @@
 package dev.skrilltrax.chip8
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import dev.skrilltrax.chip8.model.DisplayMatrix
+import dev.skrilltrax.chip8.model.DisplayMatrix.Companion.COLUMNS
+import dev.skrilltrax.chip8.model.DisplayMatrix.Companion.ROWS
+
+private const val SCALE = 100
+
+@Composable
+fun Render(modifier: Modifier, displayMatrix: DisplayMatrix, scale: Int = SCALE) {
+  Canvas(modifier) {
+    for (y in 0 until ROWS) {
+      for (x in 0 until COLUMNS) {
+        val color = if (displayMatrix.getPixel(x, y)) Color.Black else Color.White
+        drawRect(
+          color = color,
+          topLeft = Offset((x * scale).toFloat(), (y * scale).toFloat()),
+          size = Size(scale.toFloat(), scale.toFloat())
+        )
+      }
+    }
+  }
+}
