ffe220580931db85a3d4812c53aefa72dd74a2e6
Aditya Wasan <adityawasan55@gmail.com>
2022-01-16 21:02:30 +0530

    feat(keyboard): add `Keyboard` class
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- common/src/commonMain/kotlin/dev/skrilltrax/chip8/Keyboard.kt
+++
@@ -1,2 +0,0 @@
-package dev.skrilltrax.chip8
-
---
+++ common/src/commonMain/kotlin/dev/skrilltrax/chip8/keyboard/Keyboard.kt
@@ -0,0 +1,12 @@
+package dev.skrilltrax.chip8.keyboard
+
+class Keyboard {
+
+  fun isKeyPressed(key: Int): Boolean {
+    TODO()
+  }
+
+  fun waitForKeyPress(): Int {
+    TODO()
+  }
+}
