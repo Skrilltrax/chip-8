21f8a79d8efc954b7a4c693f17011baf15f93a99
Aditya Wasan <adityawasan55@gmail.com>
2022-01-21 15:10:45 +0530

    feat(desktop): load ROM from filesystem
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,26 +1,58 @@
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.remember
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import dev.skrilltrax.chip8.display.Render
-import dev.skrilltrax.chip8.display.DisplayMatrix
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.window.*
+import dev.skrilltrax.chip8.Chip8
+import java.awt.FileDialog
+import java.io.File
 
 fun main() = application {
-  Window(onCloseRequest = ::exitApplication) {
+  val state = rememberWindowState(placement = WindowPlacement.Maximized)
+
+  Window(onCloseRequest = ::exitApplication, state = state) {
     MaterialTheme {
-      val displayMatrix = remember { DisplayMatrix.createEmpty() }
-      SideEffect {
-        displayMatrix.setPixel(14, 15)
-        displayMatrix.setPixel(14, 16)
-        displayMatrix.setPixel(14, 17)
-        displayMatrix.setPixel(14, 18)
-        displayMatrix.setPixel(14, 19)
-        displayMatrix.setPixel(14, 20)
+      var isFileSelected by remember { mutableStateOf(false) }
+      var filePath by remember { mutableStateOf("") }
+      val chip8 = remember { Chip8() }
+      val windowWidth = LocalDensity.current.run { state.size.width.toPx() }
+      val scale = (windowWidth / 64).toInt()
+
+      if (isFileSelected) {
+        SideEffect { chip8.loadRom(File(filePath).readBytes()) }
+
+        chip8.Render(scale)
+      } else {
+        FileDialogScreen(modifier = Modifier.fillMaxSize()) {
+          isFileSelected = true
+          filePath = it
+        }
       }
-      Render(Modifier.fillMaxSize(), displayMatrix, 20)
     }
   }
+}
+
+@Composable
+fun FrameWindowScope.FileDialogScreen(modifier: Modifier = Modifier, onFileSelected: (String) -> Unit) {
+  Box(modifier, contentAlignment = Alignment.Center) {
+    Button(onClick = {
+      val files = openFileDialog()
+      onFileSelected(files.first().absolutePath)
+    }) {
+      Text(text = "Open File Picker")
+    }
+  }
+}
+
+fun FrameWindowScope.openFileDialog(): List<File> {
+  val fileDialog = FileDialog(window).apply {
+    isMultipleMode = false
+    isVisible = true
+  }
+  return fileDialog.files.toList()
 }
